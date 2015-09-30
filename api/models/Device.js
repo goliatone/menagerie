69b50d5860d442782562e58e6f4aebc19c9127e7
goliatone <burgosemi@gmail.com>
2015-09-29 22:12:44 -0400

    Models: Added Device

---
+++ api/models/Device.js
@@ -0,0 +1,35 @@
+/**
+* Device.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+    attributes: {
+        uuid: {
+            type: 'string',
+            primaryKey: true,
+            required: true
+        },
+        name: {
+            type: 'string'
+        },
+        description: {
+            type: 'string'
+        },
+        status: {
+            type: 'string'
+        },
+        type:{
+            model:'deviceType'
+        },
+        location: {
+            model: 'location'
+        },
+        configuration: {
+            model: 'configuration'
+        }
+    }
+};
