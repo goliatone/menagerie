f36dca66559407a8954c72d20a3698c1d250a9f2
goliatone <burgosemi@gmail.com>
2015-10-02 18:28:24 -0400

    WIP device controller

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -43,7 +43,7 @@ module.exports = {
             },
             form:{
                 action: '/api/device',
-                method:'post'
+                method:'POST'
             }
         };
 
