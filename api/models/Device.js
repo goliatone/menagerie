da4bec9fd5636b0ac1138d7b6919de815b012337
goliatone <burgosemi@gmail.com>
2015-10-04 23:40:21 -0400

    Model inherit from BaseModel

--- api/models/Device.js
+++ api/models/Device.js
@@ -4,9 +4,10 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
-var uuid = require('random-uuid-v4');
-var parseModel = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil').parseModel;
-module.exports = {
+var extend = require('gextend');
+var BaseModel = require('../../lib/BaseModel');
+
+var Device = {
     autoPK: true,
     nicename: 'Device',
     attributes: {
@@ -33,35 +34,8 @@ module.exports = {
         configuration: {
             model: 'configuration'
         }
-    },
-    getEmptyObject: function(req){
-        var Model = parseModel(req);
-        var record = {},
-            value;
-
-        Object.keys(Model.attributes).map(function(key){
-            record[key] = null;
-        });
-        record.uuid = uuid().toUpperCase();
-        return record;
-    },
-    getParametersFromRequest: function(req){
-        var Model = parseModel(req);
-        var record = {},
-            value;
-
-        Object.keys(Model.attributes).map(function(key){
-            value = req.param(key);
-            if(!value) return;
-            record[key] = value;
-        });
-        return record;
-    },
-    findByIdFromRequest: function(req){
-        var Model = parseModel(req);
-        var pk = Model.primaryKey;
-        var query = {};
-        query[pk] = req.param(pk);
-        return Model.findOne(query);
     }
 };
+
+
+module.exports = extend({}, BaseModel, Device);
