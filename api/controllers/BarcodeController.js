c3f07ad7677adcb1fcc0614dbdfbba7ec2201441
goliatone <burgosemi@gmail.com>
2015-10-06 15:56:04 -0400

    BarcodeController handle 404 assets

--- api/controllers/BarcodeController.js
+++ api/controllers/BarcodeController.js
@@ -5,27 +5,21 @@ var fs = require('fs');
 
 module.exports = {
     index: function(req, res){
-        var text = req.param('content'),
-            filename = 'qr_' +  Date.now();
+        var content = req.param('content'),
+            filename = req.param('filename');
 
-        var img = qr.encodePng(text, {
-            dotSize:5,
-            foregroundColor: 0xFFFFFF,
-            backgroundColor: 0x0
-        });
-
-        //We probably want to save this in S3 and just
-        //redirect...
-        var uri = '/images/qrs/' + filename + '.png';
-
-        fs.writeFile('./assets' + uri, img.data, function(err) {
-              if(err) return res.send(err);
-              res.redirect(uri);
+        BarcodeService.createQRCode(content, filename).done(function(){
+            var uri = BarcodeService.getImagePath(filename);
+            res.redirect(uri);
+        }).catch(function(err){
+            return res.send(err);
         });
     },
     serve: function(req, res){
         var uri = req.params.filename,
-            png = fs.createReadStream('./assets/images/qrs/' + uri);
+            png = BarcodeService.getImageStream(uri);
+
+        if(!png) return res.notFound('404');
         //we should check to see if we actually have the file :/
         var mimeType = 'image/png';
         res.writeHead(200, { 'Content-Type': mimeType });
