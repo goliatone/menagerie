619339b764b01df15265fe19714b2c0c7dd2b3af
goliatone <burgosemi@gmail.com>
2015-10-08 09:32:41 -0400

    LocationService updatez

--- api/services/LocationService.js
+++ api/services/LocationService.js
@@ -1,20 +1,23 @@
-var contactsRepo = require('../../init-data/location.js');
+'use strict';
+
+var datasource = require('../../init-data/location.js');
 //import contactsList from '/init-data/contacts';
 
 module.exports = {
-    preloadData: function (_next) {
-        console.log(">>>>>>>>>>>>>>> preloading data.......");
+    preloadData: function (data, cb) {
+        console.log('>>>>>>>>>>>>>>> preloading data.......');
 
-        Contact.findOrCreateEach(["uuid"], contactsRepo.records).then(function (_records) {
-            console.log("Contact created: " + JSON.stringify(_records));
-            if (_next) {
-                _next(_records);
-            }
+        Location.findOrCreateEach(['uuid'], data).then(function (records) {
+            console.log('Location created: ' + JSON.stringify(records));
+            if (cb) cb(null, records);
         }).catch(function (err) {
-            console.error("Error on LocationService.preloadData");
+            console.error('Error on LocationService.preloadData');
             console.error(err);
             console.error(JSON.stringify(err));
+            if(cb) cb(err, null);
         });
-
+    },
+    preloadDataFromSeed:function(cb){
+        module.exports.preloadData(datasource.records, cb);
     }
 };
