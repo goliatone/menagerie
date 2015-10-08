64091d40ccee90ff7cc780885f75dd1757122609
goliatone <burgosemi@gmail.com>
2015-10-08 00:20:34 -0400

    TODO: Add background job for CSV processing

--- api/services/CSVService.js
+++ api/services/CSVService.js
@@ -2,7 +2,8 @@ var Converter = require('csvtojson').Converter;
 var converter = new Converter({});
 
 module.exports = {
-
+    //todo, add as background job to tie in CSV upload
+    //and file conversion.
     toJSON: function(filepath){
 
         converter.on('end_parsed', function(json){
