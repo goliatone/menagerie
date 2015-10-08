5c97254138372402101eafa20d47d2d17f0a36c8
goliatone <burgosemi@gmail.com>
2015-10-08 00:05:15 -0400

    WIP csv converter

--- api/services/CSVService.js
+++ api/services/CSVService.js
@@ -1,4 +1,15 @@
+var Converter = require('csvtojson').Converter;
+var converter = new Converter({});
+
 module.exports = {
 
-    toJSON: function()
+    toJSON: function(filepath){
+
+        converter.on('end_parsed', function(json){
+            console.log('CSV', json);
+        });
+
+        //handle errors here
+        require('fs').createReadStream(filepath).pipe(converter);
+    }
 };
