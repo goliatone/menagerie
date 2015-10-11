68430b7b77cf2ae0417b3b259831838a7a3b0ccb
goliatone <burgosemi@gmail.com>
2015-10-11 19:22:41 -0400

    CSVService PoC on how to handle parsing files on job

--- api/services/CSVService.js
+++ api/services/CSVService.js
@@ -1,16 +1,30 @@
 var Converter = require('csvtojson').Converter;
 var converter = new Converter({});
+var Promise = require('bluebird');
 
 module.exports = {
     //todo, add as background job to tie in CSV upload
     //and file conversion.
     toJSON: function(filepath){
 
-        converter.on('end_parsed', function(json){
-            console.log('CSV', json);
+        var result = new Promise(function(resolve, reject){
+            var out = [];
+            converter.on('record_parsed', function(record){
+                //here we should clean up our keys.
+                out.push(record);
+            });
+
+            converter.on('end_parsed', function(json){
+                resolve(out);
+            });
+            converter.on('error', function(err){
+                reject(err);
+            });
         });
 
         //handle errors here
         require('fs').createReadStream(filepath).pipe(converter);
+
+        return result;
     }
 };
