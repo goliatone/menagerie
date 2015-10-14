0222048a5bc97d0ffdd7b5d0eba3f156a34ea3ca
goliatone <burgosemi@gmail.com>
2015-10-14 17:19:30 -0400

    ConfigurationController extend BaseController

--- api/controllers/ConfigurationController.js
+++ api/controllers/ConfigurationController.js
@@ -4,8 +4,10 @@
  * @description :: Server-side logic for managing configurations
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+ var BaseController = require('../../lib/BaseController')('Configuration');
+ var debug = require('debug')('controller:Configuration');
+ var extend = require('gextend');
 
-module.exports = {
-	
-};
+ var Controller = {};
 
+ module.exports = extend({}, BaseController, Controller);
