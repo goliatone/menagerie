664f9abd340e5546a08221d2636416e439294a3d
goliatone <burgosemi@gmail.com>
2015-10-16 11:04:47 -0400

    Added JSON import...make generic...

--- api/services/LocationService.js
+++ api/services/LocationService.js
@@ -21,5 +21,40 @@ module.exports = {
     },
     preloadDataFromSeed:function(cb){
         module.exports.preloadData(datasource.records, cb);
+    },
+    preloadFromJSONExport: function(){
+        //TODO: Ensure that we are following a schema
+        console.log('Preload data from JSON Export');
+
+        var datasource = {locations:[]};
+
+        try {
+            datasource = require('../../init-data/location-export-json');
+        } catch(e){
+            console.log('Error importing datasource');
+            return;
+        }
+
+        var data = [], record;
+        datasource.locations.map(function(raw){
+            data.push({
+                uuid: raw.roomUuid.toUpperCase(),
+                name: [raw.bldg, pad(raw.level, 3), raw.roomNumber].join('-'),
+                description: 'Room Number ' + raw.roomNumber + ', cable id ' + raw.cableId
+            });
+        });
+
+        function pad(char, len){
+            var mask = '000';
+            return (mask + char).slice(-1 * mask.length);
+        }
+
+        console.log('data: ', data);
+        console.log('Preloading data...');
+        module.exports.preloadData(data).then(function(){
+            console.log('Complete');
+        }).catch(function(err){
+            console.log('Error');
+        });
     }
 };
