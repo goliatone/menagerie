56f0c9be2b7538e3ff37d1ee0db39b584df266e3
goliatone <burgosemi@gmail.com>
2015-10-28 18:11:42 -0400

    updae env config development

--- config/env/development.js
+++ config/env/development.js
@@ -17,9 +17,9 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
   models: {
-    //   connection: 'localDiskDb'
+      connection: 'localDiskDb'
     //   connection: 'productionPostgres'
-      connection: 'developmentPostgres',
+    //   connection: 'developmentPostgres',
 
   }
 };
