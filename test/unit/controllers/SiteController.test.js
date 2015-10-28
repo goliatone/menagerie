e048f578891030e4b89de96ed769f2a0d016f928
goliatone <burgosemi@gmail.com>
2015-10-28 15:10:37 -0400

    Adding controller unit test

---
+++ test/unit/controllers/SiteController.test.js
@@ -0,0 +1,11 @@
+require('sails-test-helper');
+
+describe(TEST_NAME, function(){
+   describe('GET index', function(){
+	it('should be successful', function(done){
+	    request.get('/dashboard')
+		.expect(200)
+		.end(done);
+	});
+   });
+});
