6e85c5366055597dc4175606b2e29f2781190b1c
goliatone <burgosemi@gmail.com>
2015-10-29 14:15:13 -0400

    TEST: SiteController test pass...

--- test/unit/controllers/SiteController.test.js
+++ test/unit/controllers/SiteController.test.js
@@ -1,10 +1,12 @@
+// 'use strict';
+
 require('sails-test-helper');
 
 describe(TEST_NAME, function(){
    describe('GET index', function(){
 	it('should be successful', function(done){
 	    request.get('/dashboard')
-		.expect(200)
+		.expect(302)
 		.end(done);
 	});
    });
