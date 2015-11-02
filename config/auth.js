0b5e3d1a5033dc8ce734d9253cdc6f57324abb26
goliatone <burgosemi@gmail.com>
2015-11-02 16:06:51 -0500

    Config auth authenticate domain

--- config/auth.js
+++ config/auth.js
@@ -2,5 +2,14 @@
 
 module.exports.auth = {
     loginRedirect: '/dashboard',
-
+    passport: {
+        authenticateDomain: function(user){
+            if(!user.email) return false;
+            var domain = user.email.split('@')[1];
+            var hostedDomain = sails.config.auth.passport.restrictToDomain;
+            hostedDomain = hostedDomain.replace('www.', '');
+            return domain === hostedDomain;
+        },
+        restrictToDomain: 'wework.com'
+    }
 };
