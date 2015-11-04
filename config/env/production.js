818fe42a1df6c79e06004e73c729753e0c716a5a
goliatone <burgosemi@gmail.com>
2015-11-04 15:05:07 -0500

    Clean up env production config file

--- config/env/production.js
+++ config/env/production.js
@@ -16,9 +16,8 @@ module.exports = {
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-
   models: {
-    connection: 'productionPostgres'
+    connection: 'remotePostgres'
   },
 
   /***************************************************************************
