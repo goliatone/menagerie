3160d5c8b59f4422b77aea17ebe955784a47622c
goliatone <burgosemi@gmail.com>
2015-11-30 13:47:38 -0500

    refactoring dbSetup

--- tasks/register/dbSetup.js
+++ tasks/register/dbSetup.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var extend = require('gextend');
+
 module.exports = function(grunt) {
 
     //TODO: We might want to do add a migrating true prop
@@ -18,19 +20,26 @@ module.exports = function(grunt) {
             }
     });
 
+    var handlers = createHanlders(grunt);
 
     grunt.registerTask('db:setup', 'Setup database', function(command) {
         var done = this.async();
-        var name = grunt.option('name');
-        var sails;
-        var sailsConfig;
 
         if (!command) {
             usage(grunt);
             return done();
         }
 
-        var args = buildArguments();
+        console.log('stdout', grunt.stdout);
+        console.log('failOnError', grunt.failOnError);
+
+        sailsLoader.loadUserConfig(function(err, out){
+
+            var args = getCommandOptions(out);
+
+            console.log('Command', command, 'args', args);
+            handlers.exec(command, args, done);
+        });
     });
 
     var _ = grunt.util._;
@@ -41,104 +50,34 @@ module.exports = function(grunt) {
      * grunt pgcreateuser --user=peperone --password=Password --roles=SUPERUSER,LOGIN,REPLICATION
      * ```
      */
-    grunt.registerTask('pg_createuser', 'Add a new Postgres user.', function() {
-        //http://www.postgresql.org/docs/current/static/sql-createuser.html
-    // grunt.registerMultiTask('pgcreateuser', 'Add a new Postgres user.', function() {
-        var self = this;
-        var data = buildArguments(self.data);
-        var done = self.async();
-
-        sailsLoader.loadUserConfig(function(err, out){
-            var stmt = 'CREATE USER ' + data.user;
-
-            if (data.roles) {
-                stmt += ' ' + data.roles.join(', ');
-            }
-
-            if (data.password) {
-                stmt += ' WITH PASSWORD \'' + data.password + '\'';
-            }
-
-            stmt += ';';
-
-            if(data.superuser) {
-                stmt += ' WITH superuser';
-            }
-
-            exec_db(data.connection, stmt, function(err, res) {
-                grunt.log.writeln('Database user "' + data.user + '" created' + (data.roles ? ' with roles: ' + data.roles.join(', ') : '') + '.');
-                done();
-            });
-        });
+    grunt.registerTask('pg_createuser', 'Add a new Postgres user.', function(){
+        var done = this.async(),
+            data = buildArguments();
+        handlers.createUser(data, done);
     });
 
-    grunt.registerTask('pg_createdb', 'Create a new Postgres database.', function() {
-        var self = this;
-        var data = self.data;
-        var done = self.async();
-
-        sailsLoader.loadUserConfig(function(err, out){
-            // do DB name and owner here:
-            // * http://www.postgresql.org/docs/8.1/static/sql-createdatabase.html
-            var stmt = 'CREATE DATABASE ' + data.name;
-
-            if (data.owner) {
-                stmt += ' WITH OWNER ' + data.owner;
-            }
-
-            if(!data.encoding) data.encoding = 'UTF-8';
-
-            stmt += 'ENCODING=\''+data.encoding+'\'';
-
-            exec_db(data.connection, stmt, function(err, res) {
-                grunt.log.writeln('Database "' + data.name + '" created.');
-                done();
-            });
-        });
+    grunt.registerTask('pg_createdb', 'Create a new Postgres database.', function(){
+        var done = this.async(),
+            data = buildArguments();
+        handlers.createDb(data, done);
     });
-
 
     grunt.registerTask('pg_owner', 'Change the owner of a Postgres database.', function() {
-        var self = this;
-        var data = self.data;
-        var done = self.async();
-
-        sailsLoader.loadUserConfig(function(err, out){
-            var stmt = 'ALTER DATABASE ' + data.name + ' OWNER TO ' + data.owner;
-            exec_db(data.connection, stmt, function(err, res) {
-                grunt.log.writeln('Database "' + data.name + '" created.');
-                done();
-            });
-        });
+        var done = this.async(),
+            data = buildArguments();
+        handlers.assignOwner(data, done);
     });
 
     grunt.registerTask('pg_dropdb', 'Drop a Postgres database.', function() {
-        var self = this;
-        var done = this.async();
-        var data = this.data;
-
-        sailsLoader.loadUserConfig(function(err, out){
-            //http://www.postgresql.org/docs/current/static/sql-dropdatabase.html
-            exec_db(data.connection, 'DROP DATABASE IF EXISTS ' + data.name + ';', function(err, res) {
-                grunt.log.writeln('Database "' + data.name + '" dropped.');
-                done();
-            });
-        });
+        var done = this.async(),
+            data = buildArguments();
+        handlers.dropDb(data, done);
     });
 
     grunt.registerTask('pg_dropuser', 'Drop a Postgres user.', function() {
-        var self = this;
-        var data = self.data;
-        var done = self.async();
-
-        sailsLoader.loadUserConfig(function(err, out){
-            var stmt = 'DROP ROLE IF EXISTS ' + data.user;
-
-            exec_db(data.connection, stmt, function(err, res) {
-                grunt.log.writeln('Database user "' + data.user + '" dropped.');
-                done();
-            });
-        });
+        var done = this.async(),
+            data = buildArguments();
+        handlers.dropUser(data, done);
     });
 
     /**
@@ -148,64 +87,27 @@ module.exports = function(grunt) {
      *       --filename='pepe.json'
      */
     grunt.registerTask('pg_sqlfile', 'Run a sql against a Postgres database', function() {
-
-        var done = this.async();
-
-        sailsLoader.loadUserConfig(function(err, out){
-
-            var connectionId = out.models.connection,
-                db = {};
-
-            if(connectionId){
-                db = out.connections[connectionId];
-            }
-
-            var exec = require('child_process').exec;
-
-            var data = buildArguments(db);
-            var dataOut = data.stdout;
-            var dataErr = data.stderr;
-
-            // var db = data.connection;
-            var command = (db.password ? 'PGPASSWORD=\'' + db.password + '\'' : '') +
-                ' psql ' +
-                ' -d ' + db.database +
-                (db.host ? ' -h ' + db.host : '' ) +
-                ' -p ' + db.port +
-                ' -U ' + db.user +
-                ' -w ' +
-                ' -f ' + data.filename;
-
-            //TODO: Add --dry-run
-            console.log(grunt.template.process(command));
-            return done();
-
-            exec(grunt.template.process(command), data.execOptions, function(err, stdout, stderr) {
-                if (stdout) {
-                    if (_.isFunction(dataOut)) {
-                        dataOut(stdout);
-                    } else if (dataOut === true) {
-                        grunt.log.write(stdout);
-                    }
-                }
-
-                if (err) {
-                    if (_.isFunction(dataErr)) {
-                        dataErr(stderr);
-                    } else if (data.failOnError === true) {
-                        grunt.fatal(err);
-                    } else if (dataErr === true) {
-                        grunt.log.error(err);
-                    }
-                }
-
-                done();
-            });
-        });//end us.loadUserConfig
+        var done = this.async(),
+            data = buildArguments();
+        handlers.sqlFile(data, done);
     });
 };
 
-function buildArguments() {
+
+function getCommandOptions(config){
+    var argv = buildArguments(),
+        defaults = {};
+
+    var connectionId = config.models.connection;
+
+    if(connectionId){
+        defaults = config.connections[connectionId];
+    }
+
+    return extend({}, defaults, argv);
+}
+
+function buildArguments(defaults) {
     var args = process.argv.slice(3),
         K = require('gkeypath');
 
@@ -219,6 +121,8 @@ function buildArguments() {
             else value = option.split('=')[1];
 
             value && (value = value.split(/,\s?/));
+
+            if(value.length === 1) value = value[0];
 
             K.set(out, key, value);
 
@@ -258,3 +162,145 @@ function exec_db(connection, statement, done) {
         });
     });
 }
+
+
+function createHanlders(grunt){
+
+    var commandMap = {
+        'create-user': createUser,
+        'create-db': createDb,
+        'assign-owner': assignOwner,
+        'drop-db': dropDb,
+        'drop-user': dropUser,
+        'sql-file': sqlFile
+    };
+
+    function exec(commandId, args, done){
+        var cmd = commandMap[commandId];
+        cmd(args, done);
+    }
+
+    function createUser(data, callback) {
+        //http://www.postgresql.org/docs/current/static/sql-createuser.html
+
+        var stmt = 'CREATE USER ' + data.user;
+
+        if (data.roles) {
+            stmt += ' ' + data.roles.join(', ');
+        }
+
+        if (data.password) {
+            stmt += ' WITH PASSWORD \'' + data.password + '\'';
+        }
+
+        stmt += ';';
+
+        if(data.superuser) {
+            stmt += ' WITH superuser';
+        }
+
+        exec_db(data.connection, stmt, function(err, res) {
+            grunt.log.writeln('Database user "' + data.user + '" created' + (data.roles ? ' with roles: ' + data.roles.join(', ') : '') + '.');
+            callback();
+        });
+    }
+
+    function createDb(data, callback) {
+        // do DB name and owner here:
+        // * http://www.postgresql.org/docs/8.1/static/sql-createdatabase.html
+        var stmt = 'CREATE DATABASE ' + data.name;
+
+        if (data.owner) {
+            stmt += ' WITH OWNER ' + data.owner;
+        }
+
+        if(!data.encoding) data.encoding = 'UTF-8';
+
+        stmt += 'ENCODING=\''+data.encoding+'\'';
+
+        exec_db(data.connection, stmt, function(err, res) {
+            grunt.log.writeln('Database "' + data.name + '" created.');
+            callback();
+        });
+    }
+
+    function assignOwner(data, callback){
+        var stmt = 'ALTER DATABASE ' + data.name + ' OWNER TO ' + data.owner;
+        exec_db(data.connection, stmt, function(err, res) {
+            grunt.log.writeln('Database "' + data.name + '" created.');
+            callback();
+        });
+    }
+
+    function dropDb(data, done){
+        //http://www.postgresql.org/docs/current/static/sql-dropdatabase.html
+        exec_db(data.connection, 'DROP DATABASE IF EXISTS ' + data.name + ';', function(err, res) {
+            grunt.log.writeln('Database "' + data.name + '" dropped.');
+            done();
+        });
+    }
+    function dropUser(data, done){
+        var stmt = 'DROP ROLE IF EXISTS ' + data.user;
+        exec_db(data.connection, stmt, function(err, res) {
+            grunt.log.writeln('Database user "' + data.user + '" dropped.');
+            done();
+        });
+    }
+
+    function sqlFile(data, done){
+        var dataOut = data.stdout;
+        var dataErr = data.stderr;
+
+        var exec = require('child_process').exec;
+
+        // var db = data.connection;
+        var command = (data.password ? 'PGPASSWORD=\'' + data.password + '\'' : '') +
+            ' psql ' +
+            ' -d ' + data.database +
+            (data.host ? ' -h ' + data.host : '' ) +
+            ' -p ' + data.port +
+            ' -U ' + data.user +
+            ' -w ' +
+            ' -f ' + data.filename;
+
+        //TODO: Add --dry-run
+        console.log(grunt.template.process(command));
+        return done();
+
+        exec(grunt.template.process(command), data.execOptions, function(err, stdout, stderr) {
+            if (stdout) {
+                if (typeof dataOut === 'function') {
+                    dataOut(stdout);
+                } else if (dataOut === true) {
+                    grunt.log.write(stdout);
+                }
+            }
+
+            if (err) {
+                if (typeof dataErr === 'function') {
+                    dataErr(stderr);
+                } else if (data.failOnError === true) {
+                    grunt.fatal(err);
+                } else if (dataErr === true) {
+                    grunt.log.error(err);
+                }
+            }
+
+            done();
+        });
+    }
+
+    return {
+        commandMap: commandMap,
+        createDb: createDb,
+        dropDb: dropDb,
+        dropUser: dropUser,
+        createUser: createUser,
+        assignOwner: assignOwner,
+        sqlFile: sqlFile,
+        exec: exec
+    };
+}
+
+
+
