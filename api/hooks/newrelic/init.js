a0cb7f4020bb9188908e64d40d2ec6875bbc6302
goliatone <burgosemi@gmail.com>
2015-12-01 15:14:21 -0500

    ...

--- api/hooks/newrelic/init.js
+++ api/hooks/newrelic/init.js
@@ -1,12 +1,12 @@
 if(process.env.NODE_ENV === 'production'){
     console.log('Initializing NewRelic agent');
     if(!process.env.NODE_NEWRELIC_KEY) console.error('NewRelic license key not found. Set NODE_NEWRELIC_KEY');
-    
+
     //TODO: should we resolve path
     var resolve = require('path').resolve;
     console.log(__dirname + '/newrelic');
-    console.log(resolve('./newrelic'))
-    
+    console.log(resolve('./newrelic'));
+
     //TODO: We should figure out how to get this path!!
     process.env.NEW_RELIC_HOME = resolve(__dirname );
     global.newrelic = require('newrelic');
