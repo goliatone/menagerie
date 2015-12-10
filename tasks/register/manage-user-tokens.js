1ea5edca7f7917d25e882d8da10af425587dabe3
goliatone <burgosemi@gmail.com>
2015-12-10 16:24:37 -0500

    clean up crud task;

--- tasks/register/manage-user-tokens.js
+++ tasks/register/manage-user-tokens.js
@@ -5,30 +5,78 @@ var buildArguments = require('../_buildArguments');
 module.exports = function (grunt) {
     var Sails = require('sails').Sails;
 
-    grunt.registerTask('menagerie:user', 'Manage user create|delete|update', function (command) {
+    grunt.registerTask('menagerie:location', 'Manage location create|delete|update|list-options', function(command){
         var done = this.async();
 
-        var env;
-
-        if (!command) {
-          usage(grunt);
-          return done();
+        if (!assertCommand(command, grunt)) return done();
+        if(command === 'describe'){
+            describeSubcommand(grunt, 'location', command, ['uuid', 'name', 'description']);
+            return done();
         }
 
-        if (grunt.option('env')){
-          env = grunt.option('env');
-        }else{
-          env = process.env.NODE_ENV;
+        var config = getConfig(grunt),
+            options = buildArguments();
+
+        var sails = new Sails();
+        sails.lift(config, function (err) {
+            var url;
+
+            if (err) {
+                grunt.fail.fatal('Could not lift sails', err);
+                return done();
+            }
+
+            exec(command, options, done);
+
+            function onDone(err, res){
+                if(err) grunt.fail.fatal(err);
+                console.log(res);
+                done(err, res);
+            }
+            function exec(command, options, done){
+                console.log('COMMAND', command, 'options', options);
+                switch(command){
+                    case 'create':
+                        Location.create({
+                            uuid: options.uuid,
+                            name: options.name,
+                            description: options.description
+                        }).exec(onDone);
+                    break;
+                    case 'update':
+                        var id = options.id;
+                            delete options.id;
+                        Location.update({
+                            id: id
+                        }, {
+                            uuid: options.uuid,
+                            name: options.name,
+                            description: options.description
+                        }).exec(onDone);
+                    break;
+                    case 'delete':
+                        Location.destroy({id: options.id}).exec(onDone);
+                    break;
+                    default:
+                        grunt.log.errorlns('Command not recognized', command);
+                        done();
+                        break;
+                }
+            }
+        });
+    });
+
+    grunt.registerTask('menagerie:user', 'Manage user create|delete|update|list-options', function (command) {
+        var done = this.async();
+
+        if (!assertCommand(command, grunt)) return done();
+        if(command === 'describe') {
+            describeSubcommand(grunt, 'user', command, ['uuid', 'name', 'description']);
+            return done();
         }
 
-        var config = {
-          port: -1,
-          log: { level: process.env.LOG_LEVEL || 'silent' },
-          environment: env,
-          migrating: true
-        };
-
-        var options = buildArguments();
+        var config = getConfig(grunt),
+            options = buildArguments();
 
         // lift Sails to get the effective configuration. We don't actually need to
         // run it, and we certainly don't want any log messages. We just want the
@@ -49,6 +97,7 @@ module.exports = function (grunt) {
                 console.log(res);
                 done(err, res);
             }
+
             function exec(command, options, done){
                 console.log('COMMAND', command, 'options', options);
                 switch(command){
@@ -80,31 +129,17 @@ module.exports = function (grunt) {
     });
 
     //Manage token:
-     grunt.registerTask('menagerie:token', 'Generate API token', function (command) {
+    grunt.registerTask('menagerie:token', 'Manage API tokens create|delete|update|list-options', function (command) {
         var done = this.async();
-        var randomToken = require('random-token').create('abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
 
-        var env;
-
-        if (!command) {
-          usage(grunt);
-          return done();
+        if (!assertCommand(command, grunt)) return done();
+        if(command === 'describe'){
+            return describeSubcommand(grunt, 'token', command, ['uuid', 'name', 'description']);
         }
 
-        if (grunt.option('env')){
-          env = grunt.option('env');
-        }else{
-          env = process.env.NODE_ENV;
-        }
-
-        var config = {
-          port: -1,
-          log: { level: process.env.LOG_LEVEL || 'silent' },
-          environment: env,
-          migrating: true
-        };
-
-        var options = buildArguments();
+        var config = getConfig(grunt),
+            options = buildArguments(),
+            randomToken = require('random-token').create('abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
 
         // lift Sails to get the effective configuration. We don't actually need to
         // run it, and we certainly don't want any log messages. We just want the
@@ -162,5 +197,46 @@ module.exports = function (grunt) {
 };
 
 function usage(grunt){
-    console.log('grunt menagerie:user:create --username=<USERNAME> --email=<EMAIL>');
+    var ln = grunt.log.writeln;
+    ln('');
+    ln('Provides CRUD interface for Location, User, and Token models.');
+}
+
+function getConfig(grunt){
+    var env;
+
+    if (grunt.option('env')) env = grunt.option('env');
+    else env = process.env.NODE_ENV;
+
+    var config = {
+        port: -1,
+        log: { level: process.env.LOG_LEVEL || 'silent' },
+        environment: env,
+        migrating: true
+    };
+
+    return config;
+}
+
+
+function assertCommand(command, grunt){
+
+    if (!command || !command.match(/create|delete|update|list-options/)) {
+        usage(grunt);
+        return false;
+    }
+
+    return true;
+}
+
+function describeSubcommand(grunt, command, subcommand, options){
+    var ln = grunt.log.writeln;
+    ln('');
+    ln('Usage:');
+    ln('$ grunt menagerie:' + command + ':' + subcommand );
+    ln('');
+    ln('Options:');
+    options.map(function(o){
+        ln('--' + o);
+    });
 }
