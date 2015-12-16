eb0192ae4f9d5cbe0b089663aaab53d0b2e64208
goliatone <burgosemi@gmail.com>
2015-12-16 14:42:46 -0500

    Migration: Added index configuration unique uuid

---
+++ migrations/20151216181941-add-index-configuration-unique-uuid.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+var TABLE;
+var KEY;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+
+    TABLE = 'configuration';
+    KEY = 'configuration_uuid_unique';
+};
+
+exports.up = function(db, callback) {
+    db.addIndex(TABLE, KEY, ['uuid'], true, callback);
+};
+
+exports.down = function(db, callback) {
+    db.removeIndex(TABLE, KEY, callback);
+};
