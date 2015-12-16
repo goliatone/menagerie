45a5c3e7a042033c5478a3b131b60c354ec79035
goliatone <burgosemi@gmail.com>
2015-12-16 11:22:17 -0500

    config.migrations use remotePostgres

--- config/migrations.js
+++ config/migrations.js
@@ -2,6 +2,8 @@
 var isDev = process.env.NODE_ENV === 'development',
     connectionId = isDev ? 'developmentPostgres' : 'remotePostgres';
 
+connectionId = 'remotePostgres';
+
 module.exports.migrations = {
   // Matches connection from config/connections.js
   connection: connectionId
