f1bb3f61aaea8edc183687840f87a9f95fff3e8c
goliatone <burgosemi@gmail.com>
2016-09-15 16:57:31 -0400

    FilesController: WIP...

--- api/controllers/FilesController.js
+++ api/controllers/FilesController.js
@@ -1,27 +1,44 @@
+'use strict';
 /**
  * FileController
+ *
+ * check how to thumbnails:
+ * 	https://github.com/bredikhin/sailsjs-skipper-thumbnail-example
  *
  * @description :: Server-side logic for managing files
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var BaseController = require('../../lib/BaseController')('Files');
+var debug = require('debug')('controller:Device');
+var extend = require('gextend');
 
-module.exports = {
+var Files = {
 	index: function (req,res){
 		res.writeHead(200, {'content-type': 'text/html'});
     	res.end(
-    		'<form action="http://localhost:1337/file/upload" enctype="multipart/form-data" method="post">'+
-    		'<input type="text" name="title"><br>'+
+			'<h2>Upload File</h2>' +
+    		'<form action="/files/upload" enctype="multipart/form-data" method="POST">'+
+
+			'<input type="text" name="title"><br>'+
     		'<input type="file" name="uploaded-file" multiple="multiple"><br>'+
     		'<input type="submit" value="Upload">'+
     		'</form>'
     	);
   	},
   	upload: function  (req, res) {
-    	req.file('uploaded-file').upload(function (err, files) {
+		var options = {
+			// dirname: require('path').join(sails.config.paths.tmp, 'public', 'uploads'),
+			maxBytes: 5000000, //5MB
+			onProgress: function(e){
+				sails.io.emit('/files/upload', {progress: e.percent, name: e.name});
+			}
+		};
+
+    	req.file('uploaded-file').upload(options, function (err, files) {
       		if (err) return res.serverError(err);
+
 			//Trigger command
 			sails.emit('file:upload', {files:files});
-			sails.io.emit('/file/upload', {progress: 0});
 
 			if(req.wantsJSON){
 				return res.jsonx({
@@ -29,16 +46,16 @@ module.exports = {
 	        		files: files
 	      		});
 			}
-			return res.redirect('back');
+			return res.redirect('/files');
 
     	});
   	},
 	uploadS3: function (req, res) {
-    	req.file('avatar').upload({
+    	req.file('uploaded-file').upload({
       		adapter: require('skipper-s3'),
-      		key: 'S3 Key',
-      		secret: 'S3 Secret',
-      		bucket: 'Bucket Name'
+      		key: process.env.NODE_AWS_ID,
+      		secret: process.env.NODE_AWS_SECRET,
+      		bucket: process.env.NODE_AWS_BUCKET
     	}, function (err, filesUploaded) {
       		if (err) return res.negotiate(err);
   			return res.ok({
@@ -48,3 +65,5 @@ module.exports = {
 		});
 	}
 };
+
+module.exports = extend({}, BaseController, Files);
