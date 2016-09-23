8670da8c0088c855a767ecc4cc6f24ab0c0ce672
goliatone <burgosemi@gmail.com>
2016-09-23 13:17:12 -0400

    Adding DeployedDevice model

---
+++ api/models/DeployedDevice.js
@@ -0,0 +1,20 @@
+/**
+ * DeployedDevice.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+        uuid: {
+            type: 'string',
+            // primaryKey: true,
+            required: true
+        },
+        deployment: {
+            model: 'deployment'
+        }
+    }
+};
