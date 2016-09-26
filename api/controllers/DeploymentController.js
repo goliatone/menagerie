346ce91863c8b19b4df64fcdbd567665cc2e7d9a
goliatone <burgosemi@gmail.com>
2016-09-26 19:02:33 -0400

    DeploymentController: wip adding device flow

--- api/controllers/DeploymentController.js
+++ api/controllers/DeploymentController.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * DeploymentController
  *
@@ -5,38 +7,41 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var helper = require('../../lib/relationshipUtils');
 var BaseController = require('../../lib/BaseController')('Deployment');
 var debug = require('debug')('controller:Deployment');
 var extend = require('gextend');
 
 var Controller = {
     provision: function(req, res){
+        if(!req.options.hasOwnProperty('limit')
+            && !req.wantsJSON) req.options.limit = (sails.config.blueprints.clietRecordLimit || 10);
+
         var Resource = BaseController.Resource;
 
         var Model = Resource.getModel();
 
         var id = Model.getPk(req);
 
-        var query = Model.find()
-            .where(Resource.actionUtil.parseCriteria(req))
+        var criteria = getCriteria(req);
+        console.log('criteria', criteria);
+        /*
+         * We are picking up the deployment id
+         * from the url, and messes up the criteria
+         */
+        delete criteria.id;
+
+        var query = Device.find()
+            .where(criteria)
             .limit(Resource.actionUtil.parseLimit(req))
             .skip(Resource.actionUtil.parseSkip(req))
             .sort(Resource.actionUtil.parseSort(req));
 
-        query = Resource.populateEach(query, req);
+        query.populateAll();
 
         query.exec(function found(err, records){
             if(err) return res.negotiate(err);
 
-            //this is probably not going to be called since it's
-            //not a standard get/post/push to deployment
-            if(req._sails.hooks.pubsub && req.isSocket){
-                Model.subscribe(req, records);
-                if(req.options.autoWatch) { Model.watch(req);}
-                (records || []).map(function(record){
-                    Resource.actionUtil.subscribeDeep(req, record);
-                });
-            }
             Model.findOne(id).then(function(deployment){
                 return res.ok({
                     form: {
@@ -46,6 +51,7 @@ var Controller = {
                     },
                     record: deployment,
                     records: records,
+                    criteria: criteria,
                     nicename: 'provision',
                     title: 'Details'
                 }, Resource.getViewPath('provision'));
@@ -53,7 +59,63 @@ var Controller = {
                 res.negotiate(err);
             });
         });
+    },
+    addDevices: function(req, res){
+        var id = req.param('id'),
+            ids = req.param('ids');
+
+        console.log('id', id, 'ids', ids);
+
+        //get current deployment
+        console.log('get current deployment', id);
+        Deployment.findOne(id).then(function(deployment){
+            //get all devices.
+            Device.find(ids).then(function(records){
+                //manually filter those that have an undesired state
+                var newRecords = [];
+                records.map(function(r){
+                    if(r.status !== 'inuse') newRecords.push(r);
+                });
+                //Create a DeployedDevice with status provisioned
+                return DeployedDevice.createFromDevice(newRecords, deployment.id).then(function(deployed){
+                    //Update Device status to reserved
+                    return Device.update(ids, {status:'reserved'}).then(function(updated){
+                        res.ok({ok: true, records: updated, deployed: deployed});
+                    });
+                });
+            });
+        }).catch(function(err){
+            res.negotiate(err);
+        });
+    },
+    countDevices: function(req, res){
+        var id = req.param('id');
+        console.log('count', {deployment:id});
+        DeployedDevice.count({deployment:id}).then(function(total){
+            res.ok({
+                count: total
+            });
+        }).catch(function(err){
+            res.sendError(err);
+        });
     }
 };
 
 module.exports = extend({}, BaseController, Controller);
+
+
+function getCriteria(req){
+    /*
+     * We can only pull in devices that are in specific
+     * { or:[
+         {status: {'like': 'inuse'}},
+         {status: {'like': 'not_inuse'}}
+         ]
+     }
+     */
+    var def = {where: {status: 'available'}};
+    var criteria = BaseController.Resource.actionUtil.parseCriteria(req);
+    console.log('req criteria', criteria);
+    criteria = extend(def, criteria);
+    return criteria;
+}
