71da28e94d1fd2a0147f7c7341df9a068dc2e5b7
goliatone <burgosemi@gmail.com>
2016-09-29 18:31:08 -0400

    config: adding meta config option for frontEnd

---
+++ config/front-end.js
@@ -0,0 +1,10 @@
+module.exports.frontEnd = {
+    options: {
+        locationMap: {
+            100: 'Building',
+            200: 'Floor',
+            300: 'Area/Space',
+            400: 'Room'
+        }
+    }
+};
