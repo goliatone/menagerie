1b0a4b6b75cd52b52527888db4b097f5335d3f2f
goliatone <burgosemi@gmail.com>
2016-10-03 14:42:03 -0400

    LocationController: device positioning update

--- api/controllers/LocationController.js
+++ api/controllers/LocationController.js
@@ -10,6 +10,36 @@ var BaseController = require('../../lib/BaseController')('Location');
 var debug = require('debug')('controller:Location');
 var extend = require('gextend');
 
-var Controller = {};
+var Controller = {
+    devices: function(req, res){
+        var id = req.param('id');
+        Location.findOne({id: id}).populateAll().then(function(location){
+
+            var features = [], detached = [];
+
+            (location.devices || []).map(function(device){
+                if(!device.coordinates) return detached.push({id: device.id, name: device.name, status: device.status});
+                features.push({
+                    id: device.id,
+                    name: device.name,
+                    coordinates: device.coordinates,
+                    status: device.status //we should use DeployedDevice
+                });
+            });
+
+            res.ok({
+                record: location,
+                title: 'Location Devices',
+                features: features,
+                detached: detached
+            }, BaseController.Resource.getViewPath('devices'));
+        }).catch(function(err){
+            res.sendError(err);
+        });
+    },
+    features: function(req, res){
+
+    }
+};
 
 module.exports = extend({}, BaseController, Controller);
