c4c15cd1e8d5d9676e2a2c82f56ddc9cc88c4fa8
goliatone <burgosemi@gmail.com>
2016-10-12 16:12:10 -0400

    config: update menagerie

--- config/menagerie.js
+++ config/menagerie.js
@@ -32,5 +32,15 @@ module.exports.menagerie = {
                 return classes[state];
             }
         }
+    },
+    frontEnd: {
+        options: {
+            locationMap: {
+                100: 'Building',
+                200: 'Floor',
+                300: 'Area/Space',
+                400: 'Room'
+            }
+        }
     }
 };
