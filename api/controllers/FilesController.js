a9ae362f3f4ca6787e5c712034da3ebfcc7e418a
goliatone <burgosemi@gmail.com>
2016-10-12 11:02:38 -0400

    FilesController: emit file uploaded message using entity in event type

--- api/controllers/FilesController.js
+++ api/controllers/FilesController.js
@@ -3,7 +3,7 @@
  * FileController
  *
  * check how to thumbnails:
- * 	https://github.com/bredikhin/sailsjs-skipper-thumbnail-example
+ *     https://github.com/bredikhin/sailsjs-skipper-thumbnail-example
  *
  * @description :: Server-side logic for managing files
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
@@ -13,57 +13,62 @@ var debug = require('debug')('controller:Device');
 var extend = require('gextend');
 
 var Files = {
-	index: function (req,res){
-		res.writeHead(200, {'content-type': 'text/html'});
-    	res.end(
-			'<h2>Upload File</h2>' +
-    		'<form action="/files/upload" enctype="multipart/form-data" method="POST">'+
+    index: function (req,res){
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(
+            '<h2>Upload File</h2>' +
+            '<form action="/files/upload" enctype="multipart/form-data" method="POST">'+
+            '<input type="text" name="title"><br>'+
+            '<input type="file" name="uploaded-file" multiple="multiple"><br>'+
+            '<input type="submit" value="Upload">'+
+            '</form>'
+        );
+    },
+    upload: function  (req, res) {
 
-			'<input type="text" name="title"><br>'+
-    		'<input type="file" name="uploaded-file" multiple="multiple"><br>'+
-    		'<input type="submit" value="Upload">'+
-    		'</form>'
-    	);
-  	},
-  	upload: function  (req, res) {
-		var options = {
-			// dirname: require('path').join(sails.config.paths.tmp, 'public', 'uploads'),
-			maxBytes: 5000000, //5MB
-			onProgress: function(e){
-				sails.io.emit('/files/upload', {progress: e.percent, name: e.name});
-			}
-		};
+        var options = {
+            // dirname: require('path').join(sails.config.paths.tmp, 'public', 'uploads'),
+            maxBytes: 5000000, //5MB
+            onProgress: function(e){
+                sails.io.emit('/files/upload', {progress: e.percent, name: e.name});
+            }
+        };
 
-    	req.file('uploaded-file').upload(options, function (err, files) {
-      		if (err) return res.serverError(err);
+        req.file('uploaded-file').upload(options, function (err, files) {
+              if (err) return res.serverError(err);
+            //Trigger command
+            var eventType = getEventType(req);
+            console.log('Event Type', eventType);
+            sails.emit(eventType, { files: files});
 
-			//Trigger command
-			sails.emit('file:upload', {files:files});
-
-			if(req.wantsJSON){
-				return res.jsonx({
-	        		message: files.length + ' file(s) uploaded successfully!',
-	        		files: files
-	      		});
-			}
-			return res.redirect('/files');
-
-    	});
-  	},
-	uploadS3: function (req, res) {
-    	req.file('uploaded-file').upload({
-      		adapter: require('skipper-s3'),
-      		key: process.env.NODE_AWS_ID,
-      		secret: process.env.NODE_AWS_SECRET,
-      		bucket: process.env.NODE_AWS_BUCKET
-    	}, function (err, filesUploaded) {
-      		if (err) return res.negotiate(err);
-  			return res.ok({
-    			files: filesUploaded,
-    			textParams: req.params.all()
-  			});
-		});
-	}
+            if(req.wantsJSON){
+                return res.jsonx({
+                    message: files.length + ' file(s) uploaded successfully!',
+                    files: files
+                });
+            }
+            return res.redirect('/' + (req.body.entity || 'files'));
+        });
+    },
+    uploadS3: function (req, res) {
+        req.file('uploaded-file').upload({
+            adapter: require('skipper-s3'),
+            key: process.env.NODE_AWS_ID,
+            secret: process.env.NODE_AWS_SECRET,
+            bucket: process.env.NODE_AWS_BUCKET
+        }, function (err, filesUploaded) {
+            if (err) return res.negotiate(err);
+            return res.ok({
+                files: filesUploaded,
+                textParams: req.params.all()
+            });
+        });
+    }
 };
 
 module.exports = extend({}, BaseController, Files);
+
+function getEventType(req){
+    console.log('getEventType', req.originalUrl);
+    return 'file:upload' + (req.body.entity ? (':' + req.body.entity) : '');
+}
